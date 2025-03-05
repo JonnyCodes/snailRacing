ba92fe61f6f2507bda3dd279ca893d0ec97b8173
Jonny Blackburn <jonny.blackburn@coregaming.co.uk>
2025-03-05 20:17:01 +0000

    Updates

---
+++ finished.f78ae05c.js
@@ -0,0 +1,2 @@
+const e=JSON.parse(new URLSearchParams(window.location.search).get("snailsConfig")).snails;e.sort((e,s)=>e.time-s.time),console.log(e),document.getElementById("finishers");
+//# sourceMappingURL=finished.f78ae05c.js.map
